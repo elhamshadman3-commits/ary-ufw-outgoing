ca2c71cd1c3bddbc35409c0c5bbb0f25f05fa4e0
elhamshadman3-commits <elhamshadman3@gmail.com>
2025-09-09 12:17:32 +0200

    Update ary-ufw-outgoing.sh

--- ary-ufw-outgoing.sh
+++ ary-ufw-outgoing.sh
@@ -6,6 +6,7 @@ echo "[*] Allow outbound only for web + DNS..."
 ufw allow out 80/tcp
 ufw allow out 443/tcp
 ufw allow out 53/udp
+ufw allow out 53/tcp
 ufw allow out 8080/tcp
 echo "[*] Enable UFW..."
 ufw --force enable
