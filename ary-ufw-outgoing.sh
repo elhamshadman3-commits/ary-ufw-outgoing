f0503d12c6286d48e1ec64bbb8406aebd51853ce
elhamshadman3-commits <elhamshadman3@gmail.com>
2025-09-12 21:39:55 +0200

    Update ary-ufw-outgoing.sh

--- ary-ufw-outgoing.sh
+++ ary-ufw-outgoing.sh
@@ -8,6 +8,9 @@ ufw allow out 443/tcp
 ufw allow out 53/udp
 ufw allow out 53/tcp
 ufw allow out 8080/tcp
+ufw allow out 123/udp
+ufw allow out 127.0.0.53 port 53 proto udp
+ufw allow out 127.0.0.53 port 53 proto tcp
 echo "[*] Enable UFW..."
 ufw --force enable
 
