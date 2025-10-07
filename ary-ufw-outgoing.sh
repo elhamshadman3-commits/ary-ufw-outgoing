cfa86998f25642d18249d0617d9aacad37d4d472
elhamshadman3-commits <elhamshadman3@gmail.com>
2025-10-07 12:53:29 +0200

    Update ary-ufw-outgoing.sh

--- ary-ufw-outgoing.sh
+++ ary-ufw-outgoing.sh
@@ -3,8 +3,6 @@ ufw default deny outgoing
 ufw default allow routed
 
 echo "[*] Allow outbound only for web + DNS..."
-ufw allow out on lo
-ufw allow in on lo
 ufw allow out 80/tcp
 ufw allow out 443/tcp
 ufw allow out 53/udp
@@ -15,7 +13,6 @@ ufw allow out to 127.0.0.53 port 53 proto udp
 ufw allow out to 127.0.0.53 port 53 proto tcp
 ufw allow out to 127.0.0.54 port 53 proto tcp
 ufw allow out to 127.0.0.54 port 53 proto udp
-ufw allow to 127.0.0.1 port 11111 proto tcp
 ufw allow to 127.0.0.1 port 62789  proto tcp
 
 echo "[*] Enable UFW..."
