7749b08ada848511bb2327fdc1f1c5440fb97f2d
elhamshadman3-commits <elhamshadman3@gmail.com>
2025-10-12 20:56:44 +0200

    Update ary-ufw-outgoing.sh

--- ary-ufw-outgoing.sh
+++ ary-ufw-outgoing.sh
@@ -13,10 +13,17 @@ ufw allow out to 127.0.0.53 port 53 proto udp
 ufw allow out to 127.0.0.53 port 53 proto tcp
 ufw allow out to 127.0.0.54 port 53 proto tcp
 ufw allow out to 127.0.0.54 port 53 proto udp
-ufw allow to 127.0.0.1 port 62789  proto tcp
+sudo ufw allow from 127.0.0.1 to any port 53 proto tcp
+sudo ufw allow from 127.0.0.1 to any port 53 proto udp
+sudo ufw allow from 127.0.0.53 to any port 53 proto tcp
+sudo ufw allow from 127.0.0.53 to any port 53 proto udp
+sudo ufw allow from 127.0.0.54 to any port 53 proto tcp
+sudo ufw allow from 127.0.0.54 to any port 53 proto udp
+sudo ufw allow from 127.0.0.1 to any port 62789 proto tcp
 
 echo "[*] Enable UFW..."
 ufw --force enable
+sudo ufw reload
 
 echo "[*] Current UFW rules:"
 ufw status numbered
